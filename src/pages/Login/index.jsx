5e37be61ebcd8a2ca53f53899b5f796ed67856c3
Amanda Guerra <guerraamanda1@hotmail.com>
2023-01-20 21:48:07 -0300

    validação de usuário e senha

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -11,9 +11,25 @@ export const Login = () => {
   const navigate = useNavigate();
   const [title, setTitle] = useState("Login");
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState('');
+  const [users, setUsers] = useState([
+    {
+      username: "joao",
+      password: "oidevs",
+    },
+    {
+      username: "jady",
+      password: "oidevs",
+    },
+  ]);
 
   const goToHome = () => {
-    navigate("/home")
+    const validUser = users.find(user => user.username === username && user.password === password);
+    if(validUser) {
+      navigate("/home")
+    } else {
+      alert('Credenciais incorretas')
+    }
   }
 
   const changeTitle = () => {
@@ -24,13 +40,17 @@ export const Login = () => {
     setUsername(e.target.value)
   }
 
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
   return (
     <div className="container">
       <div className="container-wrapper">
         <Title text={title}/>
         {username && <Subtitle text={username}/>}
-        <Input type="text" label="Usuário" value={username} onChange={handleInputChange}/>
-        <Input type="password" label="Senha"/>
+        <Input type="text" label="Usuário" onChange={handleInputChange}/>
+        <Input type="password" label="Senha" onChange={handlePasswordChange}/>
         <Button title="Entrar" onClick={goToHome}/>
         <Button title="Alterar título" onClick={changeTitle} bgColor="#454D6A"/>
         <Link title="Esqueceu sua senha?" href="https://www.google.com.br/"/>
